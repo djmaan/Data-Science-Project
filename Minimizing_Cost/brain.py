61ef8901c0004fb660989602972b76fab77a249b
djmaan <maannavdeep58@gmail.com>
2021-02-28 15:09:53 +0530

    added Minimizing the energy consumption in server  project

---
+++ Minimizing_Cost/brain.py
@@ -0,0 +1,29 @@
+# Building the Brain
+
+# Importing the libraries
+from keras.layers import Input, Dense, Dropout
+from keras.models import Model
+from keras.optimizers import Adam
+
+# BUILDING THE BRAIN
+
+class Brain(object):
+    
+    
+    def __init__(self, learning_rate = 0.001, number_actions = 5):
+        self.learning_rate = learning_rate
+        
+      
+        states = Input(shape = (3,))
+        
+        x = Dense(units = 64, activation = 'sigmoid')(states)
+        x = Dropout(rate = 0.1)(x)
+        
+        y = Dense(units = 32, activation = 'sigmoid')(x)
+        y = Dropout(rate = 0.1)(y)
+        
+        q_values = Dense(units = number_actions, activation = 'softmax')(y)
+        
+        self.model = Model(inputs = states, outputs = q_values)
+        
+        self.model.compile(loss = 'mse', optimizer = Adam(lr = learning_rate))
